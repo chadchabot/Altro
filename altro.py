8894012a21af1330e39421351446fe3b203f8726
chabotc@FECS.UOGUELPH.CA <chabotc@FECS.UOGUELPH.CA@f29819a3-0c5e-4657-b260-b16cc275aa0b>
2012-04-03 20:15:06 +0000

    simplifying the changeFocus() method

--- altro.py
+++ altro.py
@@ -631,19 +631,17 @@ def clearPanel():
     textArea.delete( 1.0, END )
 #    textArea.config( state=DISABLED )
 
-def changeFocus():
+def changeFocus( choice ):
     global eb1, eb2, eb3, eb4, eb5
-    global querySelect
-    value = querySelect.get()
-    if value == 1:
+    if choice == 1:
         eb1.focus_set()
-    elif value == 2:
+    elif choice == 2:
         eb2.focus_set()
-    elif value == 3:
+    elif choice == 3:
         eb3.focus_set()
-    elif value == 4:
+    elif choice == 4:
         eb4.focus_set()
-    elif value == 5:
+    elif choice == 5:
         eb5.focus_set()
 
 def helpWindow():
@@ -680,11 +678,11 @@ queryWindow.title( "Query Menu" )
 
 #   NOTE:   add back 'takefocus=0' if the radiobuttons aren't part of the tab sequence
 #           how to have radiobuttons show their highlightcolor or highlightbackground when they have focus?
-rb1 = Radiobutton( queryWindow, text="Find books", value=1, variable=querySelect, command=changeFocus, takefocus=0 )
-rb2 = Radiobutton( queryWindow, text="2", value=2, variable=querySelect, command=changeFocus, takefocus=0 )
-rb3 = Radiobutton( queryWindow, text="3", value=3, variable=querySelect, command=changeFocus, takefocus=0 )
-rb4 = Radiobutton( queryWindow, text="4", value=4, variable=querySelect, command=changeFocus, takefocus=0 )
-rb5 = Radiobutton( queryWindow, text="5", value=5, variable=querySelect, command=changeFocus, takefocus=0 )
+rb1 = Radiobutton( queryWindow, text="1", value=1, variable=querySelect, command=lambda: changeFocus(1), takefocus=0 )
+rb2 = Radiobutton( queryWindow, text="2", value=2, variable=querySelect, command=lambda: changeFocus(2), takefocus=0 )
+rb3 = Radiobutton( queryWindow, text="3", value=3, variable=querySelect, command=lambda: changeFocus(3), takefocus=0 )
+rb4 = Radiobutton( queryWindow, text="4", value=4, variable=querySelect, command=lambda: changeFocus(4), takefocus=0 )
+rb5 = Radiobutton( queryWindow, text="5", value=5, variable=querySelect, command=lambda: changeFocus(5), takefocus=0 )
 querySelect.set( 1 )
 
 
